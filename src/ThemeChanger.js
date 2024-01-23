db0bca9ef0229b2addabecf45bfeae8029276dd8
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2024-01-23 16:20:24 +0000

    deploiement final

--- src/ThemeChanger.js
+++ src/ThemeChanger.js
@@ -1,43 +1,56 @@
 // ThemeChanger.js
-import React, { useState, useContext } from 'react';
-import { ThemeContext } from './ThemeContext';
-import "./App.css"
-
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+import "./App.css";
 
 const ThemeChanger = () => {
   const { theme, updateTheme } = useContext(ThemeContext);
-  const [nouvelleCouleur, setNouvelleCouleur] = useState('');
-  const [nouvellePolice, setNouvellePolice] = useState('');
+  const [nouvelleCouleur, setNouvelleCouleur] = useState("blue");
+  const [nouvellePolice, setNouvellePolice] = useState("arial");
 
   const handleThemeChange = (e) => {
     e.preventDefault();
     updateTheme(nouvelleCouleur, nouvellePolice);
-    const body = document.querySelector('.App')
+    const body = document.querySelector("body");
     body.style.backgroundColor = nouvelleCouleur;
+
+    if (nouvelleCouleur === "white" || nouvelleCouleur === "gray") {
+      body.style.color = "black";
+    } else {
+      body.style.color = "white";
+    }
+
+    setNouvelleCouleur("");
+    setNouvellePolice("");
   };
 
   return (
     <div>
-      <form onSubmit={handleThemeChange} className='mt-3'>
-        <label>
-          Nouvelle Couleur :
-          <input
-            type="text"
-            value={nouvelleCouleur}
-            onChange={(e) => setNouvelleCouleur(e.target.value)}
-          />
-        </label>
+      <form onSubmit={handleThemeChange} className="mt-3">
+        <div className="d-flex justify-content-center">
+          <label className="mx-5">
+            Nouvelle Couleur :
+            <input
+              type="text"
+              value={nouvelleCouleur}
+              onChange={(e) => setNouvelleCouleur(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Nouvelle Police :
+            <input
+              type="text"
+              value={nouvellePolice}
+              onChange={(e) => setNouvellePolice(e.target.value)}
+            />
+          </label>
+        </div>
+
         <br />
-        <label>
-          Nouvelle Police :
-          <input
-            type="text"
-            value={nouvellePolice}
-            onChange={(e) => setNouvellePolice(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit" className='btn btn-success'>Changer le thème</button>
+        <button type="submit" className="btn btn-success">
+          Changer le thème
+        </button>
       </form>
     </div>
   );
